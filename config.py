6565064e7a4e75f3151d9294b64346c66db0ea59
Lewe Ohlsen <mail@lewe.io>
2018-12-17 12:14:03 +0100

    config for colab

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ env = EasyDict()        # Environment variables, set by the main program in trai
 
 tf_config['graph_options.place_pruned_graph']   = True      # False (default) = Check that all ops are available on the designated device. True = Skip the check for ops that are not used.
 #tf_config['gpu_options.allow_growth']          = False     # False (default) = Allocate all GPU memory at the beginning. True = Allocate only as much GPU memory as needed.
-env.CUDA_VISIBLE_DEVICES                       = '0,1'       # Unspecified (default) = Use all available GPUs. List of ints = CUDA device numbers to use.
+env.CUDA_VISIBLE_DEVICES                       = '0'       # Unspecified (default) = Use all available GPUs. List of ints = CUDA device numbers to use.
 env.TF_CPP_MIN_LOG_LEVEL                        = '1'       # 0 (default) = Print all available debug info from TensorFlow. 1 = Print warnings and errors, but disable debug info.
 
 #----------------------------------------------------------------------------
@@ -56,7 +56,7 @@ train.mirror_augment = False
 
 # Config presets (choose one).
 desc += '-preset-v2-2gpus'
-num_gpus = 2
+num_gpus = 1
 sched.minibatch_base = 8
 sched.minibatch_dict = {4: 256, 8: 256, 16: 128, 32: 64, 64: 32, 128: 16, 256: 8}
 sched.G_lrate_dict = {512: 0.0015, 1024: 0.002}
