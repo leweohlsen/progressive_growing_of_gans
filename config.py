a04c749c70ac0c4dfab5fe7204dfcb909ddd7c33
Lewe Ohlsen <mail@lewe.io>
2019-01-03 16:31:02 +0100

    numerical precision and initial lod

--- config.py
+++ config.py
@@ -67,6 +67,7 @@ sched.G_lrate_dict = {1024: 0.0015}
 sched.D_lrate_dict = EasyDict(sched.G_lrate_dict)
 sched.tick_kimg_base = 1
 sched.tick_kimg_dict = {}
+sched.lod_initial_resolution = 32
 
 # training parameters
 train.total_kimg = 6000
@@ -75,11 +76,11 @@ sched.lod_transition_kimg = 300
 train.image_snapshot_ticks = 10
 train.network_snapshot_ticks = 20
 
-# # Numerical precision for faster training
-# desc += '-fp16'
-# G.dtype = 'float16'
-# D.dtype = 'float16'
-# G.pixelnorm_epsilon=1e-4
-# G_opt.use_loss_scaling = True
-# D_opt.use_loss_scaling = True
-# sched.max_minibatch_per_gpu = {512: 16, 1024: 8}
+# Numerical precision for faster training
+desc += '-fp16'
+G.dtype = 'float16'
+D.dtype = 'float16'
+G.pixelnorm_epsilon=1e-4
+G_opt.use_loss_scaling = True
+D_opt.use_loss_scaling = True
+sched.max_minibatch_per_gpu = {512: 16, 1024: 8}
